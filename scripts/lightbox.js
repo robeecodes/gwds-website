0e2cf5a859848d8f03a4894ced72606480d7b362
Robin <97387960+robeecodes@users.noreply.github.com>
2023-12-10 15:18:05 +0000

    fix(scripts/lightbox.js): fallback for chrome/edge
    Sometimes .currentSrc is empty in Chrome/Edge, so this fix prevents empty lightboxes from appearing

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -1,8 +1,14 @@
 document.querySelectorAll('picture[role="button"]').forEach(pic => {
     let lightbox = document.querySelector("dialog.modal");
-    let src = pic.querySelector('img').currentSrc;
 
     pic.addEventListener("click", () => {
+        let src = pic.querySelector('img').currentSrc;
+
+        // Fallback for Chrome/Edge - on the rare occasion currentSrc is empty, the lightbox will load up the default png file
+        if (!src) {
+            src = pic.querySelector('img').src;
+        }
+
         lightbox.setAttribute("open", "true");
         lightbox.innerHTML = `<img src="${src}" /><p>Click anywhere to close</p>`;
         document.body.style.overflow = "hidden";
